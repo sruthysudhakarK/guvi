9a400a5298c9f32c8ea804adc32ce68d4ae07563
sruthysudhakarK <36394969+sruthysudhakarK@users.noreply.github.com>
2018-03-22 16:59:59 +0530

    Create NUMBER.cpp

---
+++ SET1/NUMBER.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int num;
+cin>>num;
+if(num==0)
+{
+cout<<"the number is zero";
+}
+else if(num>0)
+{
+cout<<"the number is positive";
+}
+else
+{
+cout<<"the number is negative";
+}
+}
