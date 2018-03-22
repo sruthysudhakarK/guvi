cc1bba1ab7ae9885e667e1ea9b43deb24270256b
sruthysudhakarK <36394969+sruthysudhakarK@users.noreply.github.com>
2018-03-22 17:26:08 +0530

    Create alpha.cpp

---
+++ SET1/alpha.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+int main()
+{
+	char ch;
+	cout<<"Enter a character: ";
+	cin>>ch;
+	if((ch>='a'&& ch<='z') || (ch>='A' && ch<='Z'))
+	{
+		cout<<ch<<" is an alphabet";
+	}
+	else
+	{
+		cout<<ch<<" is not an alphabet";
+	}
+}
